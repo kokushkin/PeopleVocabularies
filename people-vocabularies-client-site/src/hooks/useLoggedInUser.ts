0a3c415c16b58e5f986447b1c54a5407f69d897f
Nikolai Kulikov <nikolai.kulikov.dev@gmail.com>
2020-06-25 22:58:16 +0300

    add hooks

---
+++ people-vocabularies-client-site/src/hooks/useLoggedInUser.ts
@@ -0,0 +1,18 @@
+import { useState, useCallback } from "react";
+import { Auth } from "aws-amplify";
+
+export function useLoggedInUser() {
+  let [user, setUser] = useState(undefined);
+
+  const logOut = useCallback(async () => {
+    await Auth.signOut();
+    setUser(undefined);
+  }, []);
+
+  const getUser = useCallback(async () => {
+    const user = await Auth.currentUserInfo();
+    setUser(user);
+  }, []);
+
+  return { user, logOut, getUser };
+}
