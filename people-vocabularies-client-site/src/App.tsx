bde29bb6a800ff96f8e2361481451fd1e4e0015c
Nikolai Kulikov <nikolai.kulikov.dev@gmail.com>
2020-06-25 21:33:34 +0300

    finished with src

--- people-vocabularies-client-site/src/App.tsx
+++ people-vocabularies-client-site/src/App.tsx
@@ -1,25 +1,16 @@
-import logo from "./logo.svg";
-import "./App.css";
 import React from "react";
+import "./App.css";
+import config from "./config";
+// import {TemplatePage} from "./pages/TemplatePage";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Amplify from "aws-amplify";
+
+Amplify.configure(config.amplify);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div></div>
+    // <TemplatePage/>
   );
 }
 
