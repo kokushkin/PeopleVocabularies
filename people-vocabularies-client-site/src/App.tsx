d9fdec208b882af1195b807a17384dcdbf69dbf5
Nikolai Kulikov <nikolai.kulikov.dev@gmail.com>
2020-07-25 14:47:26 +0300

    reimport bootstrap

--- people-vocabularies-client-site/src/App.tsx
+++ people-vocabularies-client-site/src/App.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 
 import config from "./config";
-// import {TemplatePage} from "./pages/TemplatePage";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Amplify from "aws-amplify";
 import { TemplatePage } from "./pages/TemplatePage";
